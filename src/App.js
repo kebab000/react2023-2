af00976d09475e8a7d43c1d5d5388d0841ca24c6
kebab000 <dldytpq7010@gmail.com>
2023-05-10 11:39:04 +0900

    2023.05.10

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Hello(){
+    return <h1>Hello, kebab00</h1>
 }
 
-export default App;
+const element = <Hello />;
+
+ReactDOM.render(element, document.getElementById('root'));
+
+export default Hello;
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Hello(props){
+  return (
+    <div>
+      <div>{props.title1}</div>
+      <div>{props.text1}</div>
+      <div>{props.author1}</div>
+      <div>{props.author2}</div>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const name = {
+  name:"kebab00",
+  text:"hello",
+  author : {
+    name:"baby",
+    url:"naver.com"
+  }
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Hello title1={name.name} text1={name.text} author1={name.author.name} author2={name.author.url}/>)
